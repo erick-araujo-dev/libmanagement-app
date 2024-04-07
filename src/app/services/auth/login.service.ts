57dae62ad2c83ca8b42422877e7ff6730f7dd873
Erick Araujo de Carvalho <erickbyborges@gmail.com>
2024-04-07 16:50:27 -0300

    feat: submit login form

--- src/app/services/auth/login.service.ts
+++ src/app/services/auth/login.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { ApiService } from '../api/api.service';
+import { LoginResponse } from '../../types/login-response.type';
+import { tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
+  postMapping: string = "auth"
 
-  constructor() { }
+  constructor(private api: ApiService) { }
+
+  login(email: string, password: string){
+    return this.api.post<LoginResponse>(this.postMapping + "/login", { email, password }).pipe(
+      tap((value: LoginResponse) => {
+        sessionStorage.setItem("auth-token", value.token)
+      })
+    )
+  }
+
+  signup(name: string, email: string, password: string){
+    return this.api.post<LoginResponse>(this.postMapping + "/register", { name, email, password }).pipe(
+      tap((value) => {
+        sessionStorage.setItem("auth-token", value.token)
+      })
+    )
+  }
 }
